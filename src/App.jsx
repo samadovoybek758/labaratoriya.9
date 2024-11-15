8f4e4fe74b326658947c993e38f91efeda55d2d0
samadovoybek758 <oybek22samadov@gmail.com>
2024-11-15 22:59:56 +0500

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,41 @@ import up from './assets/up.png'
 
 function App() {
   const boxRef = useRef(null);
+  const numRef = useRef<HTMLSpanElement>(null)
 
   const handleClick_1 = () => {
     boxRef.current.style.transform = 'translateY(-0px)'; 
+    boxRef.current.textContent = '1'
   };
 
   const handleClick_2 = () => {
     boxRef.current.style.transform = 'translateY(-130px)'; 
+    boxRef.current.textContent = '2'
   };
 
   const handleClick_3 = () => {
     boxRef.current.style.transform = 'translateY(-250px)'; 
+    boxRef.current.textContent = '3'
   };
 
   const handleClick_4 = () => {
     boxRef.current.style.transform = 'translateY(-360px)'; 
+    boxRef.current.textContent = '4'
   }
   const handleClick_5 = () => {
     boxRef.current.style.transform = 'translateY(-470px)';
+    boxRef.current.textContent = '5'
   };
 
   const handleClick_6 = () => {
     boxRef.current.style.transform = 'translateY(-580px)';
+    boxRef.current.textContent = '6'
   };
 
 
   const handleClick_7 = () => {
     boxRef.current.style.transform = 'translateY(-670px)'; 
+    boxRef.current.textContent = '7'
   };
 
   return (
@@ -38,10 +46,10 @@ function App() {
      
       <div className="bg-gray-600 w-1/2 h-[750px] flex items-end">
         <div
-          className="w-20 h-20 bg-white mx-auto lift transition-transform duration-1000" 
+          className="w-16 h-20 bg-blue-500 mx-auto lift transition-transform text-white  flex justify-center" 
           ref={boxRef}
-          style={{ transition: 'transform 1.3s ease-in-out' }} 
-        ></div>
+          style={{ transition: 'transform 3.3s ease-in-out ' }} 
+        >1</div>
       </div>
 
       
@@ -53,42 +61,42 @@ function App() {
           >
             7
           </div>
-          <img onClick={handleClick_7} className='w-7 rounded-full' src={down} alt="" />
+          <img onClick={handleClick_7} className='cursor-pointer w-7 rounded-full' src={down} alt="" />
         </div>
 
         <div className='flex gap-2 '>
           <div onClick={handleClick_6} className="cursor-pointer bg-slate-600 w-7 flex items-center justify-center h-7 rounded-full text-white">6</div>
-          <img onClick={handleClick_6} className='w-7 rounded-full' src={down} alt="" /> 
-          <img onClick={handleClick_6} className='w-7 rounded-full' src={up} alt="" />
+          <img onClick={handleClick_6} className='cursor-pointer w-7 rounded-full' src={down} alt="" /> 
+          <img onClick={handleClick_6} className='cursor-pointer w-7 rounded-full' src={up} alt="" />
         </div>
 
         <div className='flex gap-2'>
           <div onClick={handleClick_5} className="cursor-pointer bg-slate-600 w-7 flex items-center justify-center h-7 rounded-full text-white">5</div>
-          <img onClick={handleClick_5} className='w-7 rounded-full' src={down} alt="" />
-          <img onClick={handleClick_5} className='w-7 rounded-full' src={up} alt="" />
+          <img onClick={handleClick_5} className='cursor-pointer w-7 rounded-full' src={down} alt="" />
+          <img onClick={handleClick_5} className='cursor-pointer w-7 rounded-full' src={up} alt="" />
         </div>
 
         <div className='flex gap-2'>
           <div onClick={handleClick_4} className="cursor-pointer bg-slate-600 w-7 flex items-center justify-center h-7 rounded-full text-white">4</div>
-          <img onClick={handleClick_4} className='w-7 rounded-full' src={down} alt="" />
-          <img onClick={handleClick_4} className='w-7 rounded-full' src={up} alt="" />
+          <img onClick={handleClick_4} className='cursor-pointer w-7 rounded-full' src={down} alt="" />
+          <img onClick={handleClick_4} className='cursor-pointer w-7 rounded-full' src={up} alt="" />
         </div>
 
         <div className='flex gap-2'>
           <div onClick={handleClick_3} className="cursor-pointer bg-slate-600 w-7 flex items-center justify-center h-7 rounded-full text-white">3</div>
-          <img onClick={handleClick_3} className='w-7 rounded-full' src={down} alt="" />
-          <img onClick={handleClick_3} className='w-7 rounded-full' src={up} alt="" />
+          <img onClick={handleClick_3} className='cursor-pointer w-7 rounded-full' src={down} alt="" />
+          <img onClick={handleClick_3} className='cursor-pointer w-7 rounded-full' src={up} alt="" />
         </div>
 
         <div className='flex gap-2'>
           <div onClick={handleClick_2} className="cursor-pointer bg-slate-600 w-7 flex items-center justify-center h-7 rounded-full text-white">2</div>
-          <img onClick={handleClick_2} className='w-7 rounded-full' src={down} alt="" />
-          <img onClick={handleClick_2} className='w-7 rounded-full' src={up} alt="" />
+          <img onClick={handleClick_2} className='cursor-pointer w-7 rounded-full' src={down} alt="" />
+          <img onClick={handleClick_2} className='cursor-pointer w-7 rounded-full' src={up} alt="" />
         </div>
 
         <div className='flex gap-2 items-center'>
           <div onClick={handleClick_1} className="cursor-pointer mb-9 bg-slate-600 w-7 flex items-center justify-center h-7 rounded-full text-white">1</div>
-          <img onClick={handleClick_1} className='w-7 rounded-full mb-9' src={up} alt="" />
+          <img onClick={handleClick_1} className='cursor-pointer w-7 rounded-full mb-9' src={up} alt="" />
         </div>
       </div>
     </div>
